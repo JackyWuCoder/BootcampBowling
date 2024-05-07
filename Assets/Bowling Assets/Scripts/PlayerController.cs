ba232f15079e71fb6139537724b7012864668b19
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-07 10:44:15 -0700

    Arrow Movements
    1) Constraints and without constraints.

--- Assets/Bowling Assets/Scripts/PlayerController.cs
+++ Assets/Bowling Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerMovementSpeed = 1.0f;
+    public float arrowMinPosition = -0.25f;
+    public float arrowMaxPosition = 0.25f;
     private float horizontalInput;
 
     // Start is called before the first frame update
@@ -15,7 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moving without constraints
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.position += transform.right * horizontalInput * Time.deltaTime;
+        transform.position += transform.right * horizontalInput * playerMovementSpeed * Time.deltaTime;
+
+        // Moving with constraints
+        float movePosition = Input.GetAxis("Horizontal") * playerMovementSpeed * Time.deltaTime;
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x + movePosition, arrowMinPosition, arrowMinPosition),
+                transform.position.y,
+                transform.position.z
+            );
     }
 }
