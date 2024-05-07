aa63d95ef5b74675c5a29576eb96e7313d2b1623
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-07 11:01:51 -0700

    Update PlayerController.cs

--- Assets/Bowling Assets/Scripts/PlayerController.cs
+++ Assets/Bowling Assets/Scripts/PlayerController.cs
@@ -8,12 +8,14 @@ public class PlayerController : MonoBehaviour
     public float arrowMinPosition = -0.25f;
     public float arrowMaxPosition = 0.25f;
     public Transform throwingArrow;
+    public Transform ball;
     private float horizontalInput;
+    private Vector3 ballOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballOffset = ball.position - throwingArrow.position;
     }
 
     // Update is called once per frame
@@ -27,17 +29,19 @@ public class PlayerController : MonoBehaviour
     {
         // Moving without constraints
         horizontalInput = Input.GetAxis("Horizontal");
-        throwingArrow.transform.position += throwingArrow.transform.right * horizontalInput * playerMovementSpeed * Time.deltaTime;
+        throwingArrow.position += throwingArrow.right * horizontalInput * playerMovementSpeed * Time.deltaTime;
     }
 
     private void MoveArrowWithConstraints()
     {
         // Moving with constraints
         float movePosition = Input.GetAxis("Horizontal") * playerMovementSpeed * Time.deltaTime;
-        throwingArrow.transform.position = new Vector3(
+        throwingArrow.position = new Vector3(
             Mathf.Clamp(throwingArrow.transform.position.x + movePosition, arrowMinPosition, arrowMaxPosition),
-                throwingArrow.transform.position.y,
-                throwingArrow.transform.position.z
+                throwingArrow.position.y,
+                throwingArrow.position.z
             );
+        // Set ball position based on throwing direction position
+        ball.position = throwingArrow.position + ballOffset;
     }
 }
