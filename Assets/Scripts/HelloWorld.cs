e7c086be8877c2f33f4a43c8f338da55a0c7ce48
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-07 10:18:40 -0700

    Update HelloWorld.cs

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public int numberVariable; // Variable declaration
+    public bool isDoingSomething = false; // Variable initialization
+    public float speed = 10.0f;
+    public string name = "0t";
+
+    public Transform cubeTransform;
+    public Vector3 position;
+
     // Start is called before the first frame update
     void Start()
     {
+        isDoingSomething = true; // Variable Assignment
         Debug.Log("Hello from Start");
+        Debug.Log("The new variables are " + this);
+        cubeTransform.position = position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello from Update");
+        Debug.Log("The number variable is: " + numberVariable);
+        cubeTransform.Translate(Vector3.forward);
     }
 }
