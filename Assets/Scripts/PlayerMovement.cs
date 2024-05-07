db7dbc8b354bc507430b823924fe99dadf7a0dc9
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-07 10:30:02 -0700

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -38,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void CheckForSprint()
     {
-        if (Input.GetKey(KeyCode.LeftShift)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             isSprintSpeedActive = true;
         }
