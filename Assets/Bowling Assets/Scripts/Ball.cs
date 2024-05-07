b5ab5047ddd0d84cf4517645e67651874573f7c8
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-07 16:32:36 -0700

    Ball Collision

---
+++ Assets/Bowling Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check for collision with an object
+        Debug.Log("Ball has collided with " + collision.gameObject.name);
+    }
+}
