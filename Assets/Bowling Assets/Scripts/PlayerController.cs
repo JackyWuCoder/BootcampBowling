ce113838da1da54e21ddb754d047d86ac8081268
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-16 16:51:05 -0700

    Update Pin.cs

--- Assets/Bowling Assets/Scripts/PlayerController.cs
+++ Assets/Bowling Assets/Scripts/PlayerController.cs
@@ -75,7 +75,7 @@ public class PlayerController : MonoBehaviour
     private void TryThrowBall()
     {
         // Throw the ball
-        if (Input.GetKeyDown(KeyCode.Space) && wasBallThrown)
+        if (Input.GetKeyDown(KeyCode.Space) && !wasBallThrown)
         {
             wasBallThrown = true;
             selectedBall.AddForce(throwingArrow.forward * throwForce, ForceMode.Impulse);
