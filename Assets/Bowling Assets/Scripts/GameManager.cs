0157b6d40c7123c6165d17a245f9974b32cd7b69
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-16 15:23:39 -0700

    Update GameManager.cs

--- Assets/Bowling Assets/Scripts/GameManager.cs
+++ Assets/Bowling Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
+    [SerializeField] private Pin[] pins;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,21 @@ public class GameManager : MonoBehaviour
 
     public void SetNextThrow()
     {
+        CalculateFallenPins();
         // Get the ball to the start position for throwing
         playerController.StartThrow();
+    }
+
+    public void CalculateFallenPins()
+    {
+        int count = 0;
+        foreach (Pin pin in pins)
+        {
+            if (pin.isFallen)
+            {
+                count++;
+            }
+        }
+        Debug.Log("Total Fallen Pins " + count);
     }
 }
